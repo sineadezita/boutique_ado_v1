b8e625ac74566051add36ae518402761d2a97a70
sineadezita <s.ezita@icloud.com>
2026-02-01 16:52:52 +0000

    Added checkout data caching in payment intent

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = 'django-insecure-=n35xrrh%-i_nwuf+x8i4ka(pvwm_b%9w5lx!)y71b8hoiveba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1', # vs code preview
+    'localhost', # listen for stripe webhooks 
+]
 
 
 # Application definition
@@ -185,3 +188,9 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
